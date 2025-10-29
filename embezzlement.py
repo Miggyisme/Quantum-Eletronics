eb3a20084846f16e344bc09972d51eb303aa31b1
Miguel <nickkelido@gmail.com>
2025-10-29 15:07:16 -0300

    first shot

--- embezzlement.py
+++ embezzlement.py
@@ -1,5 +1,3 @@
-import re
-
 def frac(x, y):
     return f"\\dfrac{{{x}}}{{{y}}}"
 
@@ -12,53 +10,57 @@ def H(x):
 def ket(x):
     return f"\\ket{{{x}}}"
 
+def alpha():
+    return f"\\alpha"
+
+def beta():
+    return f"\\beta"
+
 
 
 def soma(x):
-    lista = [1,2,3,4,5]
-    aberto = []
-    for x in lista:
-        aberto.append(f"{x}") 
-    aberto = "+".join(aberto)
-    return aberto
+    return f"\\left({{{x}}}\\right)"
 
-print(soma(5))
+n=5
 
+def somaboa(x):
+    ns=list(range(n+1))
+    return 
 
 
-# f"\\left({{{x}}}\\right)"
-'''
+
+
 print(
 
     frac(1,sqrt(H(5)))
     +
     soma(
 
-
+        alpha()+
         frac(1,sqrt(1))
         +
         ket("11,11")
         +"+"+
         
-
+        alpha()+
         frac(1,sqrt(2))
         +
         ket("10,10")
         +"+"+
 
-
+        alpha()+
         frac(1,sqrt(3))
         +
         ket("21,21")
         +"+"+
 
-
+        alpha()+
         frac(1,sqrt(4))
         +
         ket("20,20")
         +"+"+
 
-
+        alpha()+
         frac(1,sqrt(5))
         +
         ket("31,31")
@@ -66,7 +68,3 @@ print(
 
     )
 )
-
-
-
-'''
