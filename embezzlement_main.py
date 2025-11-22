eee17960662374554778d1ba5b93412a07a8a2d5
Miguel Abranches <nickkelido@gmail.com>
2025-11-22 12:14:42 -0300

    Implemented sqrt operation

--- embezzlement_main.py
+++ embezzlement_main.py
@@ -1,6 +1,6 @@
 from math import gcd
 ############## INSIRA O n AQUI ##############
-n = 2
+n = 3
 
 lista_de_n = list(range(1, n+1))
 binary_state = [0, 0, 1, 1]  # \ket{_0,_0} + \ket{_1,_1}
@@ -20,53 +20,52 @@ values_separados = [
 
 
 
-# Adicionando o termo da raiz.
+# Sqrt term
 master = [
     [1, i, v] for i, v in zip(lista_de_n, values_separados)
 ]
+
+
+
+
+
+# Guide list: master
 print("Guide list = ",master,"\n")
 
 
 
 
-############### LATEX #######################
-
+# Latex definitions
 def frac(x, y):
     return f"\\dfrac{{{x}}}{{{y}}}"
-
 def sqrt(x):
     return f"\\sqrt{{{x}}}"
-
 def H(x):
     return f"H_{{{x}}}"
-
 def ket(x):
     return f"\\ket{{{x}}}"
-
 def alpha():
     return f"\\alpha"
-
 def beta():
     return f"\\beta"
-
 def soma(x):
     return f"\\left({{{x}}}\\right)"
 
 
 
-############ TRABALHAR AQUI. FAZER CONTAS AQUI ############
+# Operation module
 contador = 0
 def mainprint(values_alpha_beta):
 
     for i in master:
-        # i[0] = numerador
-        # i[1] = denominador
-
-        i[0] = i[0] * 2
+        # i[0] = numerator
+        # i[1] = denominator
+        ############ OPERATION HERE ############
+        i[0] = i[0] 
         i[1] = i[1]
 
 
-        num = i[0]
+        num = i[0] * 2
         den = i[1]
         m = gcd(num, den)
         num_simpl = num // m
@@ -74,7 +73,7 @@ def mainprint(values_alpha_beta):
 
         i[0] = num_simpl
         i[1] = den_simpl
-
+        ############ OPERATION HERE ############
     return master
 
 
@@ -85,22 +84,28 @@ def mainprint(values_alpha_beta):
 
 
 
-
+# Modified List: alter
 alter = mainprint(master)
 print("Modified List =",alter,"\n")
 
 
 
 
-
-####### PRINT ######## NÃO MEXER #######
+# Print module
 def gerar_latex(master):
     termos = []
 
     for constante, n_val, pares in master:
         for a, b in pares:
+
             estados = a + ',' + b
-            termo = f"\\frac{{{sqrt(constante)}}}{{{sqrt(n_val)}}}{ket(estados)}"
+
+            # Conditional 
+            if constante == n_val:
+                termo = f"{ket(estados)}"
+            else:
+                termo = f"\\frac{{{sqrt(constante)}}}{{{sqrt(n_val)}}}{ket(estados)}"
+
             termos.append(termo)
 
     resultado = "\\left(\n" + "+\n".join(termos) + "\n\\right)"
